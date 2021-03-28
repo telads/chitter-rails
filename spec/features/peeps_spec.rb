b85544323c0160f33e7224549057b01ac8cb35d4
telads <s.telads@gmail.com>
2021-03-28 14:12:14 +0100

    used timecop successfully to test peeps in reverse chronological order

--- spec/features/peeps_spec.rb
+++ spec/features/peeps_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'peeps page displaying peeps' do
   before do
-    Timecop.freeze(Time.local(2021))
+    Timecop.freeze(Time.local(2020))
   end
 
   scenario 'peeps can be seen in reverse chronological order' do
@@ -16,12 +16,12 @@ feature 'peeps page displaying peeps' do
       visit '/'
       fill_in "neep", with: "testpeep"
       click_button 'Submit'
-      expect(page).to have_content "testpeep 2021-01-01 00:00:00 UTC"
+      expect(page).to have_content "testpeep 2020-01-01 00:00:00 UTC"
       visit '/'
-        Timecop.freeze(Time.local(2022))
+        Timecop.freeze(Time.local(2021))
       fill_in "neep", with: "testpeep2"
       click_button 'Submit'
-      expect(page).to have_content "testpeep2 2022-01-01 00:00:00 UTC"
+      expect(page).to have_content "testpeep2 2021-01-01 00:00:00 UTC"
       save_and_open_page
   end
 
