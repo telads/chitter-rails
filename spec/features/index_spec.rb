463703047b3a4f060217481eb06f20f2bea832c9
telads <s.telads@gmail.com>
2021-03-28 11:29:33 +0100

    test added: the registered user can post a message

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -18,6 +18,22 @@ feature 'index page' do
       visit '/'
       expect(page).to have_content("Sign up to Chitter!")
     end
+
+    feature 'index page' do
+      scenario 'the registered user can post a message' do
+        visit '/sign-up'
+        fill_in "user_first_name", with: "John"
+        fill_in "user_last_name", with: "Doe"
+        fill_in "user_email", with: "john@test.com"
+        fill_in "user_password", with: "test123"
+        fill_in "user_password_confirmation", with: "test123"
+        click_button "Create account"
+        visit '/'
+        click_button 'Submit'
+        expect(page).to have_content 'Your peep has been posted!'
+
+      end
+    end
   end
 
 end
